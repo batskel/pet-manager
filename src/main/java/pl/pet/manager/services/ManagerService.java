a1d61f9ddd082f16033ecf6ee42f50651f0c26bb
dbatskel <dmitry.batskel@alertinnovation.com>
2024-01-01 13:27:30 +0100

    [PET-2]: Checkstyle support

--- src/main/java/pl/pet/manager/services/ManagerService.java
+++ src/main/java/pl/pet/manager/services/ManagerService.java
@@ -3,19 +3,22 @@ package pl.pet.manager.services;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
+/**
+ * The ManagerService class provides functionality for managing managers.
+ */
 @Service
 public class ManagerService {
 
   /**
    * This method is deprecated and scheduled for removal. Please refrain from using it in new code.
    *
-   * @deprecated This method is deprecated and scheduled for removal.
    * @param name The string to be duplicated.
    * @return A string that consists of two copies of the input string concatenated together.
+   * @deprecated This method is deprecated and scheduled for removal.
    */
   @Deprecated
-  public String testMethod (@NonNull final String name){
-    return String.format("%s_%s",name, name);
+  public String testMethod(@NonNull final String name) {
+    return String.format("%s_%s", name, name);
   }
 
 }
