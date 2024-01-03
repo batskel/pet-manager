1b4b9da976d57a39b20022a84af93da7540eeb54
zenagrinkevic <mr_grin@inbox.ru>
2024-01-03 18:47:47 +0300

    [PET-12]: change the basic state of health to CustomHealthIndicator

---
+++ src/main/java/pl/pet/manager/config/CustomHealthIndicator.java
@@ -0,0 +1,13 @@
+package pl.pet.manager.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.up().withDetail("customDetail", "Everything is fine").build();
+    }
+}
