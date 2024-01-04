9b009ff6bd07ec8f46a04add6c191cd50de427f0
zenagrinkevic <mr_grin@inbox.ru>
2024-01-04 12:27:18 +0300

    [PET-12]: add javadoc

--- src/main/java/pl/pet/manager/services/CustomHealthService.java
+++ src/main/java/pl/pet/manager/services/CustomHealthService.java
@@ -1,11 +1,18 @@
-package pl.pet.manager.config;
+package pl.pet.manager.services;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+/**
+ * The CustomHealthService class provides custom logic to health check.
+ */
 @Component
-public class CustomHealthIndicator implements HealthIndicator {
+public class CustomHealthService implements HealthIndicator {
+
+    /**
+     * This method provided more detailed information about the health status of our service providers.
+     */
     @Override
     public Health health() {
         return Health.up().withDetail("customDetail", "Everything is fine").build();
